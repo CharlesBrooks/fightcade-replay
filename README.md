eb4bc45c231f12491f6128830f356a42c39d64c9
Charles Brooks <cebrooks93@gmail.com>
2024-01-13 11:49:32 -0500

    initial cut of rust implementation of fightcade replay, currently iterating over active windows and printing their titles

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # fightcade-replay
 
 Plans for #fightcade-replay
-1. Angular + electron app to automatically record fightcade games
+1. Rust app using winapi to automatically record fightcade games
 2. Setup shortcuts for the user to "clip" rounds
 3. setup configuration to allow users to save games to different directories
 
-Bonus goal : 
-Find a way to automatically store set sessions by player name / some identifier 
+
+Basically I don't really know what im doing, so im trying to scrap something together to get to a point where i can find a fightcade game window, and record it to a file.
+
+Ideally I'll figure out how to write x amount of video to a clips file for a given user.
+
+Baby steps :
+1. Always find a 3rd Strike window for now since 3rd Strike is the best fighting game of all time.
+2. Maybe figure out a way to screenshot the 3rd strike first, then work on trying to do a video. 
---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use windows::{
+    Win32::Foundation::*, 
+    Win32::UI::WindowsAndMessaging::*,
+};
+use windows::Win32::UI::WindowsAndMessaging::{GetWindowTextW, GetWindowTextLengthW};
+use std::ffi::OsString;
+use std::os::windows::ffi::OsStringExt;
+
+unsafe extern "system" fn enum_windows_callback(hwnd: HWND, lparam: LPARAM) -> BOOL {
+    let mut title_length = GetWindowTextLengthW(hwnd) as usize;
+    if title_length > 0 {
+        title_length += 1; // For null terminator
+        let mut title: Vec<u16> = vec![0; title_length];
+        GetWindowTextW(hwnd, &mut title);
+
+        let title_osstring = OsString::from_wide(&title[..title_length - 1]);
+        if let Ok(title_string) = title_osstring.into_string() {
+            let titles = &mut *(lparam.0 as *mut Vec<String>);
+            titles.push(title_string);
+        }
+    }
+    true.into()
+}
+
+fn get_window_titles() -> Vec<String> {
+    let mut titles: Vec<String> = Vec::new();
+    unsafe {
+        EnumWindows(Some(enum_windows_callback), LPARAM(&mut titles as *mut _ as isize));
+    }
+    titles
+}
+
+fn main() {
+    let window_titles = get_window_titles();
+    for title in window_titles {
+        println!("Window : {}", title);
+    }
+}
